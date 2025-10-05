9ebd2f6827f087c957feb20af303c957cccfd8ba
BlkMambaGod <willyoukna@hotmail.com>
2025-10-04 23:06:44 -0400

    Completed all getters and setters of the classes

--- Source/Traffic_Lights/Traffic_Lights.ino
+++ Source/Traffic_Lights/Traffic_Lights.ino
@@ -1,43 +1,72 @@
 #include <pitches.h>
 
-class Beeper {
+class Beeper { // The beeping module class
   private:
-  int pin;
+  static const int pin = 10;
+  int button;
 
   public:
-  Beeper(int pin) : pin(pin) {}
+  Beeper(int button) : button(button) {}
+  
+  // getters & setters
+  int getPin() const { return pin; }
 
-  void setup() {
+  // void setPin(int pin) { this->pin = pin; }
+
+  int getButton() const { return button; }
+
+  void setButton(int button) { this->button = button; }
+
+  // methods
+  void setup() const {
     pinMode(pin, OUTPUT);
+    pinMode(button, INPUT);
   }
 
-  void beep(int note, int freq) {
+  void beep(int note, int freq) const {
+    tone(pin, note, freq);
+  }
+
+  void signal(int note, int freq) const {
     tone(pin, note, freq);
   }
 };
 
-class Countdown {
+class Countdown { // The 7 segment module fot the countdown
   private: 
-  int pin1, pin2, pin3;
-  
+  int dataPin, latchPin, clockPin;
+  static const int DIGITS[10] = {0B11111100, 0B01100000, 0B11011010, 0B11110010, 0B01100110, 0B10110110, 0B10111110, 0B11100000, 0B11111110, 0B11110110};
   
   public:
-  Countdown(int pin1, int pin2, int pin3) :
-            pin1(pin1), pin2(pin2), pin3(pin3) {}
+  Countdown(int dataPin, int latchPin, int clockPin) :
+            dataPin(dataPin), latchPin(latchPin), clockPin(clockPin) {}
   
-  void setup() {
-    pinMode(pin1, OUTPUT);
-    pinMode(pin1, OUTPUT);
-    pinMode(pin1, OUTPUT);
-  }
-      
   //getters
+  int getDataPin() const { return dataPin; }
+
+  int getLatchPin() const { return latchPin; }
+
+  int getClockPin() const { return clockPin; }
   
   //setters
+  void setDataPin(int dataPin) { this->dataPin = dataPin; }
 
-  //count
+  void setLatchPin(int latchPin) { this->latchPin = latchPin; }
 
-  //hurry
+  void setClockPin(int clockPin) { this->clockPin = clockPin;}
+
+  //methods
+
+  void count() {}
+
+
+  void setup() {
+    pinMode(dataPin, OUTPUT);
+    pinMode(latchPin, OUTPUT);
+    pinMode(clockPin, OUTPUT);
+  }
+      
+
 };
 
 class Car_lights {
@@ -93,14 +122,13 @@ class Ped_lights {
   private:
   int white, red;
   static Countdown timer;
-  static Beeper beep;
-  unsigned long start;
+  Beeper beep;
   unsigned long blink;
   bool red_state;
 
   
   public:
-  Ped_lights(int white, int red) : white(white), red(red) {
+  Ped_lights(int white, int red, Beeper beep) : white(white), red(red), beep(beep) {
     red_state = false;
   }
   
@@ -111,19 +139,31 @@ class Ped_lights {
     blink = millis();
   }
   //getters
-  unsigned long getStart() const {
-    return start;
-  }
-
   int getWhite() const { return white; }
 
   int getRed() const { return red; }
+
+  Beeper getBeep() const { return beep; }
+
+  Countdown getTimer() const { return timer; }
+
+  unsigned long getBlink() const { return blink; }
+
+  bool getRed_state() const { return red_state; }
+
+
   
   //setters
-  unsigned long setStart(unsigned long time) {
-    start = time;
-  }
+  void setBlink(unsigned long time) { blink = time; }
 
+  void setWhite(int white) { this->white = white; }
+
+  void setRed(int red) { this->red = red; }
+
+  void setRed_state(bool state) { red_state = state; }
+
+
+  //Methods
   void go() const {
     digitalWrite(white, true);
     digitalWrite(red, false);
@@ -131,8 +171,10 @@ class Ped_lights {
 
   void hurry() {
     digitalWrite(white, false);
+
+    // blinking the light
       unsigned long now = millis();
-      if (now - blink >= 500) {
+      if (now - blink >= 1000) {
         blink = now;
         if (red_state == true)
           red_state = false;
@@ -140,7 +182,10 @@ class Ped_lights {
           red_state = true;
         digitalWrite(red, red_state);
         beep.beep(NOTE_C6, 100);
+        
+        // timer.count();
       }
+
 
   }
 
@@ -153,75 +198,94 @@ class Ped_lights {
 
 class Traffic_lights {
   private:
-  Car_lights c_alpha;
-  Ped_lights p_alpha;
-  Car_lights c_beta;
-  Ped_lights p_beta;
+  Car_lights car;
+  Ped_lights ped;
+
  
   public:
-  Traffic_lights(Car_lights c_alpha, Ped_lights p_alpha, Car_lights c_beta,
-                  Ped_lights p_beta) : c_alpha(c_alpha),
-                  p_alpha(p_alpha), c_beta(c_beta), p_beta(p_beta) {}
+  Traffic_lights(Car_lights car, Ped_lights ped) : car(car), ped(ped) {}
     
   //getters
-  
+  Car_lights getcar() const { return car; }
+
+  Ped_lights getped() const { return ped; }
+
   //setters
+  void setcar(Car_lights car) { this->car = car; }
 
-  //go
+  void setped(Ped_lights ped) { this->ped = ped; }
 
-  //hurry
+  //Methods
+  void setup() {
+    car.setup();
+    ped.setup();
+  }
 
-  //stop
+  void go() const {
+    car.go();
+    ped.go();
+  }
+
+  void hurry() const {
+    car.hurry();
+    ped.hurry();
+  }
+
+  void stop() const {
+    car.stop();
+    ped.stop();
+  }
 };
 
+
+
+
 unsigned long start;
-unsigned long blink;
-Beeper Ped_lights::beep(10);
+
+Beeper beep_one(9);
+Beeper beep_two(8);
+
 Countdown Ped_lights::timer(9, 12, 13);
-Car_lights c_verti(49, 50, 51);
-Ped_lights p_verti(52, 53);
+// Car_lights c_verti(49, 50, 51);
+// Ped_lights p_verti(52, 53);
 // bool red_state = false;
 
 
 void setup() {
   start = millis();
   // unsigned long Ped_lights::blink = millis();
-  c_verti.setup();
-  p_verti.setup();
+  // c_verti.setup();
+  // p_verti.setup();
 }
  
 void loop() { 
   unsigned long now = millis();
 
   if (now - start < 5000) {
-    c_verti.go();
+    // c_verti.go();
     // p_verti.go();
     //beta = stop
   }
   else
     if (now - start < 8000) {
-      c_verti.hurry();
-      p_verti.hurry();
+      // c_verti.hurry();
+      // p_verti.hurry();
       //beta = stop
     }
     else
       if (now - start < 13000) {
-        c_verti.stop();
+        // c_verti.stop();
         // p_verti.stop();
         //beta = go
       }
       else
         if ( now - start < 16000) {
-          c_verti.freak_on();
+          // c_verti.freak_on();
           //beta = hurry
         }
         else {
           // restart the process
-          c_verti.freak_off();
+          // c_verti.freak_off();
           start = now;
         }
 }
-
-
-
-
